f0f8a003515e0fe2f871dda07b797ab2155319b0
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-05 09:32:37 -0600

    first commit learning methods C#

---
+++ Learning methods/Learning methods/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine("Color?");
+var color = Console.ReadLine(); 
+Console.WriteLine("Animal?");
+var animal = Console.ReadLine();
+Console.WriteLine("Band?");
+var band = Console.ReadLine();
+Console.WriteLine($"{name} has many interesting things about them." +
+                  $"For starters {color} is their fav color." +
+                  $"Their fav animal is {animal}." +
+                  $"And Lastly {band} is who they like to listent too.");
