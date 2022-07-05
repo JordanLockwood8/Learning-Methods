7a9826e2a90415584e7c128f9b92719554eeba16
Jordan Lockwood <8xavienlockwood8@gmail.com>
2022-07-05 11:30:14 -0600

    Add new project named Math

---
+++ Math/Math/Methods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    internal class Methods
+    {
+        public static int Add(int num1, int num2)
+        {
+            var sum = num1 + num2;
+            return sum;
+        }
+
+        public static int Sub(int num1,int num2)
+        {
+            var sub = num1 - num2;
+            return sub;
+
+        }
+        public static int Mul(int num1,int num2)
+        {
+            var mul = num1 * num2;
+            return mul;
+        }
+        public static int Div(int num1, int num2)
+        {
+            var div = num1 / num2;
+            return div;
+        }
+
+
+
+    }
+
+ }
---
+++ Math/Math/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Math;
+
+Console.WriteLine("learning methods!");
+
+var answer = Methods.Add(2, 2);
+Console.WriteLine(answer);
+
+var answer2 = Methods.Sub(30, 3);
+Console.WriteLine(answer2);
+
+var answer3 = Methods.Mul(4, 4);
+Console.WriteLine(answer3);
+
+var answer4 = Methods.Div(50, 5);
+Console.WriteLine(answer4);
